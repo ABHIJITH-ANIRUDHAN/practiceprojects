69b378d01811ae5ba6ff4a8af2efb26ace9c47eb
ABHIJITH-ANIRUDHAN <abhijithanirudhan@proton.me>
2022-12-02 10:08:17 +0530

    clock demo

---
+++ index.js
@@ -0,0 +1,8 @@
+const hourEl = document.querySelector(".hour");
+st minuteEl = document.querySelector(".minute");
+st secondEl = document.querySelector(".second ");
+function updateClock(){
+    const curentDate = new Date();
+    console.log(currentDate);
+}
+updateClock();
